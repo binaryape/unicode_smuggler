aa8fa256769e36fd813a20ca2b5b584d6080eaa1
Pete Birkinshaw <pete@binary-ape.org>
2025-04-27 17:56:00 +0100

    A full mix, ready for publishing

--- mix.exs
+++ mix.exs
@@ -6,8 +6,29 @@ defmodule UnicodeSmuggler.MixProject do
       app: :unicode_smuggler,
       version: "0.1.0",
       elixir: "~> 1.16",
+      description: "Hide text in Unicode and maybe find hidden text in Unicode",
+      package: package(),
+      name: "UnicodeSmuggler",
+      source_url: "https://github.com/binaryape/unicode_smuggler",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      test_coverage: [
+        tool: ExCoveralls
+      ],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      docs: [
+        main: "readme",
+        #logo: "logo.png",
+        extras: ["README.md", "LICENSE"]
+      ],
+      deps: deps(),
+      compilers: Mix.compilers(),
+      elixirc_paths: elixirc_paths(Mix.env)
     ]
   end
 
@@ -30,4 +51,18 @@ defmodule UnicodeSmuggler.MixProject do
       {:doctor, "~> 0.21", only: :dev, runtime: false}
     ]
   end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/binaryape/unicode_smuggler"
+      }
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support", "priv"]
+  defp elixirc_paths(_), do: ["lib", "priv"]
+
 end
